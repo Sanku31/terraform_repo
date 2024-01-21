da1f19b7ed3ace9b2829d4d3492b1f584898c266
Sanku31 <135406398+Sanku31@users.noreply.github.com>
2024-01-21 15:17:49 +0530

    Update eip.tf

--- eip.tf
+++ eip.tf
@@ -1,3 +1,12 @@
 /*resource "aws_eip" "myeip" {
   vpc = true
 }*/
+
+resource "aws_s3_bucket" "example" {
+  bucket = "mk_bucket_3197"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
